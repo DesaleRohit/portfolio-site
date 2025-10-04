06e62a9a7eacc8c236b35895595a8cdb0765cbf4
DesaleRohit <rohitdesale2005@gmail.com>
2025-10-04 21:36:20 +0530

    [MODIFIED]:Rohit - first try with GSAP animation appllied on hero-section and tagline

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+// Hero section animation
+gsap.from('.hero-title', {
+    duration: 1.5,
+    y: -100,
+    opacity: 0,
+    ease: 'bounce.out'
+});
+
+gsap.from('.tagline', {
+    duration: 1.5,
+    y: 100,
+    opacity: 0,
+    delay: 0.5,
+    ease: 'power2.out'
+});
